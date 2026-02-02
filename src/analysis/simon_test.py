f06c90843c7188a76c04139a1aac521ca47d028f
Jules <jules.delrieu@gmail.com>
2026-02-02 17:20:55 +0100

    comment test

--- src/analysis/simon_test.py
+++ src/analysis/simon_test.py
@@ -4,18 +4,19 @@ from notebooks.get_raw_data import response
 from src.db.es_connection import ESConnection
 
 
-def main():d
-
+def main():
+    # initialisation de la connexion à la base de données
     es_conn = ESConnection()
 
-
+    # création de la requête personnalisée
 
     personnalise_query = {
         "query":{
         
         }
     }
+    # exécution de la requête
     response = es_conn.es.search(index="debats_assemblee_nationale", body=personnalise_query)
 
-    es_conn.get_word_count_for_year(date_seance="2022-01-03", field="texte")
-    
+    # comptage des mots pour l'année 2022
+    count =es_conn.get_word_count_for_year(date_seance="2022-01-03", field="texte")
